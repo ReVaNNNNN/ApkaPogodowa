522f771548d0f15913c0c7a7a8c56388fe31cf1d
ReVaN <revannn5480@gmail.com>
2020-07-10 09:29:38 +0200

    Fetching data from OpenWeather APi

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,68 @@ import Result from "./Result";
 
 class App extends Component {
   state = {
+    value: "",
+    date: "",
     city: "",
+    sunrise: "",
+    sunset: "",
+    temp: "",
+    pressure: "",
+    wind: "",
+    error: false,
   };
 
   handleChangeInput = (e) => {
     this.setState({
-      city: e.target.value,
+      value: e.target.value,
     });
+  };
+
+  handleCitySubmit = (e) => {
+    e.preventDefault();
+
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=f4649408805b935cfa225d7616053ecd&units=metric`;
+
+    fetch(API)
+      .then((response) => {
+        if (response.ok) {
+          return response;
+        }
+
+        throw Error("Wystąpił błąd !");
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        const date = new Date().toLocaleString;
+
+        this.setState((prevState) => ({
+          error: false,
+          date: date,
+          city: prevState.value,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          temp: data.main.temp,
+          pressure: data.main.pressure,
+          wind: data.wind,
+        }));
+      })
+      .catch((err) => {
+        this.setState({
+          error: true,
+          city: this.state.value,
+        });
+      });
   };
 
   render() {
     return (
       <div className="App">
-        <Form city={this.state.city} onChange={this.handleChangeInput} />
-        <Result />
+        <Form
+          city={this.state.value}
+          onChange={this.handleChangeInput}
+          submit={this.handleCitySubmit}
+        />
+        <Result weather={this.state} />
       </div>
     );
   }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Result = () => {
-  return <div>Miasto, pogoda</div>;
+const Result = (props) => {
+  const {
+    error,
+    city,
+    temp,
+    date,
+    sunrise,
+    sunset,
+    pressure,
+    wind,
+  } = props.weather;
+
+  return (
+    <>
+      <div>Pogoda dla: {city}</div>
+      <div>Temperatura: {temp}</div>
+    </>
+  );
 };
 
 export default Result;
